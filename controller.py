4193b53a2f2381f35c57fe6b8b9edf9e85280bf4
Zaktansnake <zaktansnake@outlook.com>
2017-11-19 22:48:56 +0800

    Upadted for Assignment Tasks
    Updated python files for assignment required tasks.

--- controller.py
+++ controller.py
@@ -1,6 +1,6 @@
 '''
-Please add your name:
-Please add your matric number: 
+Please add your name: David Chong Yong Ming
+Please add your matric number: A0116633L
 '''
 
 import sys
@@ -23,35 +23,174 @@ class Controller(EventMixin):
     def __init__(self):
         self.listenTo(core.openflow)
         core.openflow_discovery.addListeners(self)
+        self.macToPort = {}
+        # For Premium Service Class
+        self.psc = {}
+        return
         
     # You can write other functions as you need.
-        
     def _handle_PacketIn (self, event):    
-    	# install entries to the route table
+        # install entries to the route table
+        packet = event.parsed
+        dpid = event.dpid
+        port = event.port
+        source = packet.src
+        destination = packet.dst
+
         def install_enqueue(event, packet, outport, q_id):
-          
+            log.info("Installing flow for %s:%i -> %s:%i", source, port, destination, outport)
+            message = of.ofp_flow_mod()
+            message.match = of.ofp_match.from_packet(packet, port)
+            message.actions.append(of.ofp_action_output(port = outport, queue_id = q_id))
+            message.data = event.ofp
+            message.priority = 1000
+            event.connection.send(message)
+            log.info("Message with queue ID %i sent via port %i\n", q_id, outport)
+            return
 
-    	# Check the packet and decide how to route the packet
+        # Check the packet and decide how to route the packet
         def forward(message = None):
+            log.info("Receiving packet %s from port %i", packet, port)
 
+            # Store the port from where the packet comes from
+            self.macToPort[dpid][source] = port
+
+            # Get source and destination IP address
+            sourceip = None
+            destinationip = None
+
+            if packet.type == packet.IP_TYPE:
+                log.info("Packet is IP type %s", packet.type)
+                ippacket = packet.payload
+                sourceip = ippacket.srcip
+                destinationip = ippacket.dstip
+            elif packet.type == packet.ARP_TYPE:
+                log.info("Packet is ARP type %s", packet.type)
+                arppacket = packet.payload
+                sourceip = arppacket.protosrc
+                destinationip = arppacket.protodst
+            else:
+                log.info("Packet is Unknown type %s", packet.type)
+
+            # Check if source and destination ip is in same premium service class
+            qid = 0
+
+            if sourceip == None or destinationip == None:
+                qid = 0
+            elif isSameClass(sourceip, destinationip):
+                qid = 1
+            else:
+                qid = 2
+
+            # If multicast, flood
+            if destination.is_multicast:
+                flood("Multicast to %s -- flooding" % (destination))
+                return
+
+            # If destination port is not found, flood
+            if destination not in self.macToPort[dpid]:
+                flood("Port for %s unknown -- flooding" % (destination))
+                return
+
+            outport = self.macToPort[dpid][destination]
+            install_enqueue(event, packet, outport, qid)
+            return
+
+        # Check if IPs belong to same premium service class
+        def isSameClass(sourceip, destinationip):
+            for i in self.psc[dpid]:
+                if sourceip in i and destination in i:
+                    log.info("Source IP %s and Destination IP %s are in the same Premium Service Class", sourceip, destinationip)
+                    return True
+            log.info("Source IP %s and Destination IP %s are not in the same Premium Service Class", sourceip, destinationip)
+            return False
 
         # When it knows nothing about the destination, flood but don't install the rule
         def flood (message = None):
-        
-        
+            log.info(message)
+            floodmsg = of.ofp_packet_out()
+            floodmsg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+            floodmsg.data = event.ofp
+            floodmsg.in_port = port
+            event.connection.send(floodmsg)
+            log.info("Flood Message sent via port %i\n", of.OFPP_FLOOD)
+            return
+
         forward()
+        return
 
 
     def _handle_ConnectionUp(self, event):
         dpid = dpid_to_str(event.dpid)
         log.debug("Switch %s has come up.", dpid)
-        
+
+        self.macToPort[dpid] = {}
+        self.psc[dpid] = []
+
+        filename = "policy.in"
+        filereader = open(filename, "r")
+        firstline = filereader.readline().split(' ')
+
+        numofpolicies = int(firstline[0])
+        numofpsc = int(firstline[1])
+        fpolicies = []
+
+        for i in xrange(numofpolicies):
+            line = filereader.readline().strip().split(',')
+            source = line[0]
+            destination = line[1]
+            port = line[2]
+            fpolicies.append((source, destination, port))
+
+        for j in xrange(numofpsc):
+            line = filereader.readline().strip().split(',')
+            self.psc[dpid].append(line)
+
+        log.info("Premium Service Class List: %s", self.psc[dpid])
+
         # Send the firewall policies to the switch
         def sendFirewallPolicy(connection, policy):
-            
 
-        for i in [FIREWALL POLICIES]:
+            # From first host to second host
+            source = policy[0]
+            destination = policy[1]
+            port = policy[2]
+
+            messageone = of.ofp_flow_mod()
+            messageone.priority = 2000
+            messageone.actions.append(of.ofp_action_output(port = of.OFPP_NONE))
+            messageone.match.dl_type = 0x800
+            messageone.match.nw_proto = 6
+            messageone.match.nw_src = IPAddr(source)
+            messageone.match.nw_dst = IPAddr(destination)
+            messageone.match.tp_dst = int(port)
+            connection.send(messageone)
+            log.info("Firewall Policy: source = %s, destination = %s, port = %s", source, destination, port)
+
+            # From second host to first host
+            source = policy[1]
+            destination = policy[2]
+            port = policy[2]
+
+            messagetwo = of.ofp_flow_mod()
+            messagetwo.priority = 2000
+            messagetwo.actions.append(of.ofp_action_output(port=of.OFPP_NONE))
+            messagetwo.match.dl_type = 0x800
+            messagetwo.match.nw_proto = 6
+            messagetwo.match.nw_src = IPAddr(source)
+            messagetwo.match.nw_dst = IPAddr(destination)
+            messagetwo.match.tp_dst = int(port)
+            connection.send(messagetwo)
+            log.info("Firewall Policy: source = %s, destination = %s, port = %s", source, destination, port)
+            return
+
+        for i in fpolicies:
             sendFirewallPolicy(event.connection, i)
+
+        for j in self.psc:
+            pass
+
+        return
             
 
 def launch():
--- mininetTopo.py
+++ mininetTopo.py
@@ -1,6 +1,6 @@
 '''
-Please add your name:
-Please add your matric number: 
+Please add your name: David Chong Yong Ming
+Please add your matric number: A0116633L
 '''
 
 import os
@@ -16,22 +16,65 @@ from mininet.node import RemoteController
 net = None
 
 class TreeTopo(Topo):
-			
-	def __init__(self):
-		# Initialize topology
-		Topo.__init__(self)        
-	
-	# You can write other functions as you need.
 
-	# Add hosts
+    def __init__(self):
+        # Initialize topology
+        Topo.__init__(self)
+
+        filename = "topology.in"
+        filereader = open(filename, "r")
+        firstline = filereader.readline().split(' ')
+
+        numofhosts = int(firstline[0])
+        numofswitches = int(firstline[1])
+        numoflinks = int(firstline[2])
+
+        # Add Hosts
+        hosts = []
+
+        for i in xrange(numofhosts):
+            host = self.addHost('H%d' % (i+1))
+            hosts.append(host)
+
+        print hosts
+
+        # Add Switches
+        switches = []
+
+        for j in xrange(numofswitches):
+            sconfig = {'dpid': "%016x" % (j+1)}
+            switch = self.addSwitch('S%d' % (j+1), **sconfig)
+            switches.append(switch)
+
+        print switches
+        print self.switches()
+
+        # Add Links
+        self.linkConfigs = []
+
+        for k in xrange(numoflinks):
+            link = filereader.readline().strip().split(',')
+            print link
+            self.linkConfigs.append(link)
+            firstnode = link[0]
+            secondnode = link[1]
+            # Links are added without bandwidth as bandwidth is added in the queue
+            self.addLink(firstnode, secondnode)
+
+        print self.links(True, False, True)
+
+
+    # You can write other functions as you need.
+
+    # Add hosts
     # > self.addHost('h%d' % [HOST NUMBER])
 
-	# Add switches
+    # Add switches
     # > sconfig = {'dpid': "%016x" % [SWITCH NUMBER]}
     # > self.addSwitch('s%d' % [SWITCH NUMBER], **sconfig)
 
-	# Add links
-	# > self.addLink([HOST1], [HOST2])
+    # Add links
+    # > self.addLink([HOST1], [HOST2])
 
 def startNetwork():
     info('** Creating the tree network\n')
@@ -45,12 +88,45 @@ def startNetwork():
     info('** Starting the network\n')
     net.start()
 
+    def getLinkSpeed(firstnode, secondnode):
+        for i in topo.linkConfigs:
+            if firstnode == i[0] and secondnode == i[1]:
+                return int(i[2]) * 1000000
+
+        return 0
+
+    networkints = 0
+
     # Create QoS Queues
     # > os.system('sudo ovs-vsctl -- set Port [INTERFACE] qos=@newqos \
     #            -- --id=@newqos create QoS type=linux-htb other-config:max-rate=[LINK SPEED] queues=0=@q0,1=@q1,2=@q2 \
     #            -- --id=@q0 create queue other-config:max-rate=[LINK SPEED] other-config:min-rate=[LINK SPEED] \
     #            -- --id=@q1 create queue other-config:min-rate=[X] \
     #            -- --id=@q2 create queue other-config:max-rate=[Y]')
+
+    # Get Switch interfaces
+    for j in topo.links(True, False, True):
+        for k in topo.switches():
+            linkinfo = j[2]
+            for l in [1, 2]:
+                if linkinfo["node%i" % (l)] == k:
+                    networkints += 1
+                    port = linkinfo["port%i" % (l)]
+                    firstnode = linkinfo["node1"]
+                    secondnode = linkinfo["node2"]
+                    linkspeed = getLinkSpeed(firstnode, secondnode)
+                    xspeed = 100000000 # 100 mbps
+                    yspeed = 50000000 # 50 mbps
+                    interface = "%s-eth%s" % (k, port)
+
+                    # OS System Call
+                    os.system("sudo ovs-vsctl -- set Port %s qos=@newqos \
+                            -- --id=@newqos create QoS type=linux-htb other-config:max-rate=%i queues=0=@q0,1=@q1,2=@q2 \
+                            -- --id=@q0 create queue other-config:max-rate=%i other-config:min-rate=%i \
+                            -- --id=@q1 create queue other-config:min-rate=%i \
+                            -- --id=@q2 create queue other-config:max-rate=%i" % (interface, linkspeed, linkspeed, linkspeed, xspeed, yspeed))
+
+    print "QoS set up on %i interfaces" % (networkints)
 
     info('** Running CLI\n')
     CLI(net)
